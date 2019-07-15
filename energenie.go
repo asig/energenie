948b517d535ca7c57e871b76c2ed7f4ff61dbd0b
Andreas Signer <asigner@gmail.com>
2019-07-16 00:52:32 +0200

    fixed comment

--- energenie.go
+++ energenie.go
@@ -4,7 +4,6 @@
  *
  * Copyright (c) 2018 Andreas Signer <asigner@gmail.com>
  *
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
